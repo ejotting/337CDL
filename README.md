be7f8d25a8e10928b763e51ee766a62de60ca05a
ejotting <ejotting@purdue.edu>
2026-03-28 03:18:53 -0400

    Added AHB Module

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 337CDL
 ECE 33700 Cooperative Design Lab
+
+Please make files and modules as necessary and sync to repository
---
+++ ahb_usb.sv
@@ -0,0 +1,25 @@
+`timescale 1ns / 10ps
+
+module ahb_usb (
+    input logic clk,
+    input logic n_rst,
+    input logic hsel,
+    input logic [3:0]haddr,
+    input logic [1:0]htrans,
+    input logic [1:0]hsize,
+    input logic hwrite,
+    input logic [31:0]hwdata,
+    
+    output logic [31:0]hrdata,
+    output logic hready,
+    output logic hresp,
+
+    input logic dp_in,
+    input logic dm_in,
+    
+    output logic dp_out,
+    output logic dm_out,
+    output logic d_mode
+);
+
+endmodule
